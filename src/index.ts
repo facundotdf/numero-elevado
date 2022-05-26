6460adf166f9629e51592fcbbbe45ba12d5c244f
facundotdf <104038338+facundotdf@users.noreply.github.com>
2022-05-26 06:31:48 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let base: number = Number(prompt(" ingrese numero de base"));
+let exponente: number = Number(prompt(" ingrese numero de exponente"));
+
+function calcularResultado(base: number, exponente: number): number {
+  let resultado: number = 1;
+  exponente <= 0;
+  for (let i = 1; i <= exponente; i++) {
+    resultado = resultado * base;
+  }
+  return resultado;
+}
+console.log(
+  "el calculo de la potencia es: ",
+  calcularResultado(base, exponente)
+);
